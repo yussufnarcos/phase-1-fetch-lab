716e7c2344c47f7370aaf51ed999a9f4d4985fb9
yussufnarcos <narcosyussuf@gmail.com>
2025-03-20 10:04:44 +0300

    phase-1-fetch-lab

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => {
+          renderBooks(data); 
+          return data; 
+      })
+      .catch(error => console.error("Error fetching books:", error));
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener("DOMContentLoaded",function(){
+  getfetchbooks()
+})
